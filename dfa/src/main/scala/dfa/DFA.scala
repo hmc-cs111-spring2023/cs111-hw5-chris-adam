5ff6f3f0278dde82bdd3e416c2ea4cedb72c0a85
Adam B <76700849+AdamrBeckwith@users.noreply.github.com>
2023-02-19 00:15:19 +0000

    Co-authored-by: cgcouto <cgcouto@users.noreply.github.com>

--- dfa/src/main/scala/dfa/DFA.scala
+++ dfa/src/main/scala/dfa/DFA.scala
@@ -1,3 +1,27 @@
 package dfa // leave this line in the file
 
 // TODO: replace this comment with your implementation
+
+
+case class State(label: String)
+
+case class Transition(from: State, to: State, symbol: Char)
+
+class DFA(states: Set[State], transitions: Set[Transition], startState: State, acceptingStates: Set[State]):
+    def accepts(input: String) = 
+        var currentState = startState
+        var currentInput = ""
+        var result = false
+        for (i <- input) {
+            var validTransitions = transitions.filter((n:Transition) => (n.from == currentState && n.symbol == i))
+            if (validTransitions.size != 1) then println("NonValid DFA!!!!")
+            currentState = validTransitions.head.to
+        }
+
+        // check if we're in a accepting state
+        if (acceptingStates.contains(currentState)) then true
+        else false
+            
+    // for loop it (over length of input)
+    // pull off relevant character of input
+    // 
